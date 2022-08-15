5a19b6e9b38ebaf6557ee8ef77cd461f8445c1be
Guilherme Sa <guicarrer@yahoo.com.br>
2022-08-15 18:48:11 -0300

    Commit Inicial

---
+++ Secao_05/rest-with-spring-boot-and-java-erudio/src/test/java/br/com/xmetrocubo/restwithspringbootandjavaerudio/StartupTests.java
@@ -0,0 +1,13 @@
+package br.com.xmetrocubo.restwithspringbootandjavaerudio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
