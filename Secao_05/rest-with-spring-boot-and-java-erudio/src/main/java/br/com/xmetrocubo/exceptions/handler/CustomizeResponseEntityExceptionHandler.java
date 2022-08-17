18050b96cb70f169ee3b95a851049e5ceddee03e
Guilherme Sa <guicarrer@yahoo.com.br>
2022-08-17 12:04:25 -0300

    Sessao 08

--- Secao_05/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/xmetrocubo/exceptions/handler/CustomizeResponseEntityExceptionHandler.java
+++ Secao_05/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/xmetrocubo/exceptions/handler/CustomizeResponseEntityExceptionHandler.java
@@ -1,7 +1,7 @@
 package br.com.xmetrocubo.exceptions.handler;
 
 import br.com.xmetrocubo.exceptions.ExceptionResponse;
-import br.com.xmetrocubo.exceptions.UnsupportedMathOperationException;
+import br.com.xmetrocubo.exceptions.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -22,10 +22,10 @@ public class CustomizeResponseEntityExceptionHandler extends ResponseEntityExcep
         return new ResponseEntity<>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
-    @ExceptionHandler(UnsupportedMathOperationException.class)
-    public final ResponseEntity<ExceptionResponse> handleBadRequestExceptions(Exception ex, WebRequest request){
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public final ResponseEntity<ExceptionResponse> handleNotFOunExceptions(Exception ex, WebRequest request){
         ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
-        return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.NOT_FOUND);
     }
 
 }
