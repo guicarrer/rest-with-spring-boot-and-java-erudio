9979899d70aec1d6af380cfcb0b972a49e5bc67e
Guilherme Sa <guicarrer@yahoo.com.br>
2022-08-17 18:01:18 -0300

    Sessao 11 - Adicionando Suporte a Migrations com Flyway

---
+++ Secao_05/rest-with-spring-boot-and-java-erudio/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,8 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+    (1,'Brasil','Guilherme','Male','Sa'),
+    (2,'Brasil','Ayrton','Male','Senna'),
+    (3,'Brasil','Leonardo','Male','da Vinci'),
+    (5,'Porbandar','Indira','Male','Gandhi'),
+    (7,'Kentucky - US','Muhammad','Male','Ali'),
+    (9,'Africa do Sul','Nelson','Male','Mandela'),
+    (10,'Croacia','Nikola','Male','Tesla');
